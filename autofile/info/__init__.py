b14f2d9d3c8dccfe0d1c6afe17bc35a177fc4da7
snelliott <sarah.elliott26@uga.edu>
2019-10-10 12:38:19 -0500

    update scan save system from ints to grid_vals

--- autofile/info/__init__.py
+++ autofile/info/__init__.py
@@ -2,6 +2,7 @@
 """
 from autofile.info._info import object_
 from autofile.info._info import string
+from autofile.info._info import dict_
 from autofile.info._info import from_string
 from autofile.info._info import matches_function_signature
 from autofile.info._info import Info
@@ -9,6 +10,7 @@ from autofile.info._info import Info
 __all__ = [
     'object_',
     'string',
+    'dict_',
     'from_string',
     'matches_function_signature',
     'Info',
