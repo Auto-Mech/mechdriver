d82a6dce187581bcf60f36f693e44ff82e768af5
Kevin Moore <kmoore@blueslogin1.lcrc.anl.gov>
2019-10-19 08:50:09 -0500

    update

--- estoktpdriver/run.py
+++ estoktpdriver/run.py
@@ -6,6 +6,9 @@ import read_dat
 # Read the run parameters from a datafile
 PARAMS = read_dat.params('params.dat')
 
+print(PARAMS.HIND_INC, type(PARAMS.HIND_INC))
+
+
 for attr in dir(PARAMS):
     if '__' not in attr:
         print(attr, getattr(PARAMS, attr))
