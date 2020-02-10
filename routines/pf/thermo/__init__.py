566b155330f82fab125bbe652d6963750a06e96a
Kevin Moore <kmoore@blueslogin2.lcrc.anl.gov>
2020-02-10 15:25:20 -0600

    update

---
+++ routines/pf/thermo/__init__.py
@@ -0,0 +1,16 @@
+"""
+Interfaces to codes to compute thermochemical paramaeters
+"""
+
+from thermo import heatform
+from thermo import nasapoly
+from thermo import runner
+from thermo import util
+
+
+__all__ = [
+    'heatform',
+    'nasapoly',
+    'runner',
+    'util'
+]
---
+++ routines/pf/thermo/thermp.mako
@@ -0,0 +1,9 @@
+1, 0
+Nwell, Nprod
+30
+nt
+${deltaH}   ${enthalpyT}
+${formula}
+${composition_str}
+**
+${breakT}
