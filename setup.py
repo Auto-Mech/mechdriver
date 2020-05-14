968f327da98d7661a939b4ce03299e1038184a41
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-05-13 21:14:13 -0500

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-""" Install moldriver
-"""
+''' Install moldriver
+'''
 from distutils.core import setup
 
 
-setup(name="moldr",
-      version="0.1.1",
-      packages=["moldr",
-                "autofile",
-                "autofile.info",
-                "autofile.file",
-                "autofile.system"])
+setup(name='moldriver',
+      version='0.1.0',
+      packages=['drivers',
+                'routines',
+                'routines.es',
+                'routines.es._routines',
+                'routines.es.runner',
+                'routines.pf',
+                'routines.pf.ktp',
+                'routines.pf.thermo',
+                'routines.pf.messf',
+                'routines.pf.runner',
+                'lib',
+                'lib.amech_io',
+                'lib.amech_io.parser',
+                'lib.amech_io.writer',
+                'lib.filesys',
+                'lib.phydat',
+                'lib.reaction'])
