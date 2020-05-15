54e4e42badebca7e9e9d0ceac30e0dfb7820d55b
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2020-05-14 22:22:22 -0500

    add line to submission scripts to remove core dumps

--- lib/submission.py
+++ lib/submission.py
@@ -89,72 +89,87 @@ class _EnterDirectory():
 # DCT OF DEFAULT SUBMISSION STRINGS
 PROJROT = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "RPHt.exe >& /dev/null"
 )
 MESSPF = (
     "#!/usr/bin/env bash\n"
     "export OMP_NUM_THREADS=10\n"
+    "ulimit -c 0\n"
     "messpf pf.inp pf.out >> stdout.log &> stderr.log"
 )
 MESSRATE = (
     "#!/usr/bin/env bash\n"
     "export OMP_NUM_THREADS=10\n"
+    "ulimit -c 0\n"
     "mess mess.inp rate.out >> stdout.log &> stderr.log"
 )
 VARECOF = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "/home/ygeorgi/build/rotd/multi >& varecof.out"
 )
 MCFLUX = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "/home/ygeorgi/build/rotd/mc_flux"
 )
 TSTCHECK = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "/home/ygeorgi/build/rotd/tst_check"
 )
 THERMP = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "THERMP FILE"
 )
 PAC99 = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "PACC << EOF\n"
     "FORMULA\n"
     "EOF"
 )
 DSARRFIT = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "dsarrfit.x_cfg"
 )
 G09 = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "g09 run.inp run.out >> stdout.log &> stderr.log"
 )
 PSI4 = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "psi4 -i run.inp -o run.out >> stdout.log &> stderr.log"
 )
 MOLPRO = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "molpro -n 4 run.inp -o run.out "
     "--nouse-logfile --no-xml-output >> "
     "stdout.log &> stderr.log"
 )
 MOLPRO_MPPX = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "molpro --mppx -n 4 run.inp -o run.out "
     "--nouse-logfile --no-xml-output >> "
     "stdout.log &> stderr.log"
 )
 MOLPRO_MREF = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "molpro -n 8 run.inp -o run.out "
     "--nouse-logfile --no-xml-output >> "
     "stdout.log &> stderr.log"
 )
 MOLPRO_MREF_MPPX = (
     "#!/usr/bin/env bash\n"
+    "ulimit -c 0\n"
     "molpro --mppx -n 12 run.inp -o run.out "
     "--nouse-logfile --no-xml-output >> "
     "stdout.log &> stderr.log"
--- tests/test_.py
+++ tests/test_.py
@@ -1,29 +1,30 @@
-""" Tests tasks associated with conformer tasks including
-    sampling, energy, gradients, and hessians
+""" Runs automech
 """
 
-# SCRIPT_STR = (
-#     "#!/bin/bash\n"
-#     "python -u automech.py $CWD >& run.log & disown %1"
-# )
-
-SCRIPT_STR = (
-    '#!/bin/bash\n'
-    'export CWD=/lcrc/project/CMRP/runs/data/abst2\n'
-    'export AMECHDIR=/lcrc/project/CMRP/amech\n'
-    'export OUTPUTFILE=test.log\n'
-    'ssh -n b447 " conda activate amech-env-mini                                                ;\n'
-    '               . $AMECHDIR/fake-install.sh                                                 ;\n'
-    '               cd $CWD                                                                     ;\n'
-    '               module load gaussian/09-e.01                                                ;\n' 
-    '               module load molpro/2015.1_170920                                            ;\n'
-    '               python -u $AMECHDIR/moldriver/bin/automech.py $CWD >& $OUTPUTFILE &          \n'
-    '               disown %1 "                                                                  \n'
-) 
+import os
+import subprocess
 
 
-def test__ts():
-    """ test all the ts searching  
+PATH = os.getcwd()
+INP_PATH = os.path.join(PATH, 'inp')
+RUN_DAT_PATH = os.path.join(INP_PATH, 'run.dat')
+CMD_LINE = (
+    'python -u automech.py {0} >& {0}/run.log & disown %1'.format(PATH)
+)
+
+
+def test__automech():
+    """ test automech.py
     """
 
+    # Format the run.dat with the run-save dir paths
+    with open(RUN_DAT_PATH, 'r') as file_obj:
+        run_str = file_obj.read()
+    with open(RUN_DAT_PATH, 'w') as file_obj:
+        file_obj.write(run_str.format(PATH))
 
+    subprocess.call(CMD_LINE.split())
+
+
+if __name__ == '__main__':
+    test__automech()
