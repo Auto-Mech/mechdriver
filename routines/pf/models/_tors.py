f938307ef7449c032ddf2d81ef1914b8f3fe76e0
snelliott <sarah.elliott26@uga.edu>
2020-07-31 11:26:01 -0500

    read json style database when building thermo

--- routines/pf/models/_tors.py
+++ routines/pf/models/_tors.py
@@ -292,7 +292,6 @@ def _read_hr_pot(tors_names, tors_grids, cnf_save_path,
         if constraint_dct is not None:
             locs = [constraint_dct] + locs
 
-        print('tors_path', scn_fs[-1].path(locs))
         ene = _read_tors_ene(scn_fs, locs, mod_tors_ene_info)
         if ene is not None:
             pot[point] = (ene - ref_ene) * phycon.EH2KCAL
