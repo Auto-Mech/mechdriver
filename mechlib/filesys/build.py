fd0f039c872571e8ee865adb604468d38e12b606
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-02-02 12:40:15 -0600

    update zma filesys in mechlib

--- mechlib/filesys/build.py
+++ mechlib/filesys/build.py
@@ -385,6 +385,136 @@ def zma_fs_from_prefix(prefix, zma_idxs=(0,)):
     return zma_fs, zma_path
 
 
+def zma_from_prefix(prefix, spc_dct_i, rxn_ichs=None, dirn='forw'):
+    """ get locs
+    """
+
+    # Build the zma fs
+    zma_fs = fs.zmatrix(prefix)
+
+    # Get the locs
+    # zma_locs = get_zma_locs(zma_fs, spc_dct_i, rxn_ichs=rxn_ichs, wanted_dirn=dirn)
+    zma_locs = (0,)
+
+    # print(zma_locs)
+    zma_fs[-1].create(zma_locs)
+    zma_path = zma_fs[-1].path(zma_locs)
+
+    return zma_fs, zma_path
+    # return zma_fs, zma_locs
+
+
+def get_zma_locs(zma_fs, spc_dct_i, rxn_ichs=None, wanted_dirn=('forw',)):
+    """ get locs
+        rxn = None, or (rct_name/ich, prd_name/ich)
+    """
+    
+    assert set(wanted_dirn) <= {'forw', 'rev'}
+
+    # Grab the user-input zma idx from the dct if they exist
+    dct_zma_idx = spc_dct_i.get('zma_idx', None)
+
+    if dct_zma_idx is None:
+        if rxn_ichs is None:
+            # Right now we assume that we will just grab 0th idx species
+            zma_locs = (0,)
+        else:
+            # Grab the lowest idx associated with the direction
+            zma_locs_lst = rxn_zma_locs_lst(zma_fs, rxn_ichs)
+            if zma_locs_lst:
+                want_lst = tuple(locs for locs, dirn in zma_locs_lst
+                                 if dirn in wanted_dirn)
+                zma_locs = (want_lst[0],)
+            else:
+                zma_locs = None
+    else:
+        zma_locs = (dct_zma_idx,)
+
+    return zma_locs
+
+
+def spc_zma_locs_lst(zma_fs):
+    """ Get the zma locs for the species
+    """
+    locs_lst = tuple()
+    for locs in zma_fs[-1].existing():
+        locs_lst += ((locs, None),)
+
+    return locs_lst
+
+
+def rxn_zma_locs_lst(zma_fs, rxn_ichs):
+    """ Get the zma locs that are needed
+    """
+
+    assert wanted_dirn in ('forw', 'rev', 'any')
+
+    locs_lst = tuple()
+    for locs in zma_fs[-1].existing():
+        # Not placed in the scans filesystem
+        # maybe place the vmatrix and transformation file?
+        if zma_fs[-1].file.zmatrix.exists(locs):
+            ts_zma = zma_fs[-1].file.zmatrix.read(locs)
+            frm_bnd_keys, brk_bnd_keys = zma_fs[-1].file.transformation.read(locs)
+            dirn = _chk_direction(rxn_ichs, ts_zma,
+                                  frm_bnd_keys, brk_bnd_keys)
+            locs_lst += ((locs, dirn),)
+
+    return zma_locs
+
+
+def _chk_direction(rxn_ichs, ts_zma,
+                   frm_bnd_keys, brk_bnd_keys):
+    """ Get the zma locs that are needed
+    """
+
+    fs_rct_ichs = automol.zmatrix.ts.zmatrix_reactant_inchis(
+        ts_zma, frm_bnd_keys, brk_bnd_keys, remove_stereo=False)
+    fs_rct_ichs = set(fs_rct_ichs)
+
+    fs_prd_ichs = automol.zmatrix.ts.zmatrix_product_inchis(
+        ts_zma, frm_bnd_keys, brk_bnd_keys, remove_stereo=False)
+    fs_prd_ichs = set(fs_prd_ichs)
+
+    rct_ichs, prd_ichs = rxn_ichs[0], rxn_ichs[1]
+    print('rxn ich lst check')
+    print('rct ich', set(rct_ichs))
+    print('fs rct ich', fs_rct_ichs)
+    print('prd ich', set(prd_ichs))
+    print('fs prd ich', fs_prd_ichs)
+    if set(rct_ichs) == fs_rct_ichs and set(prd_ichs) == fs_prd_ichs:
+        dirn = 'forw'
+    elif set(rct_ichs) == fs_prd_ichs and set(prd_ichs) == fs_rct_ichs:
+        dirn = 'rev'
+    else:
+        dirn = None
+
+    return dirn
+
+
+def etrans_fs_from_prefix(prefix, bath_info, thy_info):
+    """ Build an energy transfer filesystem obj
+    """
+
+    # Build the energy transfer filesys object
+    print('prefix', prefix)
+    etrans_fs = autofile.fs.energy_transfer(prefix)
+
+    # Build the energy transfer locs object
+    etrans_locs = bath_info + thy_info[1:4]
+   
+    # Create directory
+    print('etrans locs', etrans_locs)
+    etrans_fs[-1].create(etrans_locs)
+
+    return etrans_fs, etrans_locs
+
+
+# Old function that I need to get rid of
+
+
+
+
 def etrans_fs_from_prefix(prefix, bath_info, thy_info):
     """ Build an energy transfer filesystem obj
     """
