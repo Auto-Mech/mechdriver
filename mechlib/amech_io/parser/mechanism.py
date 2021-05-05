4d144310c9af5e05e3dfec0403c68fab661068e6
Luna Pratali Maffei <ac.lpratalimaffei@blueslogin1.lcrc.anl.gov>
2021-05-05 04:18:31 -0500

    sort driver moved to mechanalyzer

--- mechlib/amech_io/parser/mechanism.py
+++ mechlib/amech_io/parser/mechanism.py
@@ -5,10 +5,8 @@ Read the mechanism file
 from mechanalyzer.parser import pes
 from mechanalyzer.parser import mech
 from ioformat import ptt
-import autoparse.find as apf
 
 MECH_INP = 'inp/mechanism.dat'
-SORT_INP = 'inp/sort.dat'
 
 
 def build_pes_dct(job_path, mech_type,
@@ -47,40 +45,3 @@ def build_pes_dct(job_path, mech_type,
     pes.print_pes_channels(pes_dct)
 
     return run_pes_dct
-
-
-def read_sort_section(job_path):
-    """ reads the options for sorting
-        coverts them to two list:
-            isolate_species
-            sort_list
-        for proper input to sortdriver
-    """
-    # read the sorting input
-    sort_str = ptt.read_inp_str(job_path, SORT_INP, remove_comments='#')
-
-    submech_section = apf.all_captures(
-        ptt.end_section('isolate_submech'), sort_str)
-
-    if submech_section is None:
-        # empty section
-        isolate_species = []
-    else:
-        # format the section
-        species = apf.first_capture(
-            ptt.paren_section('species'), submech_section[0])
-        isolate_species = ptt.build_keyword_lst(species)
-
-    sortmech_section = apf.all_captures(
-        ptt.end_section('sort_mech'), sort_str)
-    # this section is mandatory
-    if sortmech_section is None:
-        print('*ERROR: sort_mech section is not defined')
-    else:
-        criteria = apf.first_capture(
-            ptt.paren_section('criteria'), sortmech_section[0])
-        n_criteria = apf.first_capture(
-            ptt.keyword_pattern('n_criteria_headers'), sortmech_section[0])
-        sort_list = ptt.build_keyword_lst(criteria+n_criteria)
-
-    return isolate_species, sort_list
