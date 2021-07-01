42975b9fa137d42675ae5c47cde7f28d0e3253e6
snelliott <sarah.elliott26@uga.edu>
2021-07-01 11:30:22 -0500

    updating proccess_task

--- drivers/procdriver.py
+++ drivers/procdriver.py
@@ -34,30 +34,21 @@ def run(pes_rlst, spc_rlst,
     # -------------------------------- #
 
     run_rlst = parser.rlst.combine(pes_rlst, spc_rlst)
+    run_lst = []
+    for _, run_lst_i in run_rlst.items():
+        run_lst.extend(list(run_lst_i))
+    # Print what is being run PESs that are being run
+    # Loop over Tasks
+    for tsk_lst in proc_tsk_lst:
 
-    for (fml, pes_idx, subpes_idx), run_lst in run_rlst.items():
+        [obj, tsk, prnt_keyword_dct] = tsk_lst
 
-        # Print what is being run PESs that are being run
-        ioprinter.runlst((fml, pes_idx, subpes_idx), run_lst)
+        spc_mod_dct_i = spc_mod_dct['global']
+        pes_mod_dct_i = pes_mod_dct['global']
 
-        # Loop over Tasks
-        for tsk_lst in proc_tsk_lst:
-
-            # Unpack the options
-            [obj, tsk, prnt_keyword_dct] = tsk_lst
-
-            # Get the model from the task
-            spc_mods, pes_mod = parser.models.extract_models(tsk)
-            spc_mod_dct_i = spc_mod_dct[spc_mods[0]]
-            pes_mod_dct_i = pes_mod_dct[pes_mod]
-
-            # Build the queue of species based on user request
-            if obj == 'spc':
-                obj_queue = parser.rlst.spc_queue(run_lst, fml)
-
-            for spc_name in obj_queue:
-                run_tsk(
-                    tsk, spc_dct, spc_name,
-                    thy_dct, prnt_keyword_dct,
-                    pes_mod_dct_i, spc_mod_dct_i,
-                    run_prefix, save_prefix)
+        if obj == 'spc':
+            run_tsk(
+                tsk, spc_dct, run_lst,
+                thy_dct, prnt_keyword_dct,
+                spc_mod_dct_i, pes_mod_dct_i,
+                run_prefix, save_prefix)
