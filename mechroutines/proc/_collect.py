7d81a4a5604c853eb5b24f14076ed33c0af4d4cf
snelliott <sarah.elliott26@uga.edu>
2021-07-02 15:04:06 -0500

    process task is functioning

---
+++ mechroutines/proc/_collect.py
@@ -0,0 +1,158 @@
+"""Collects the target info
+"""
+
+
+import thermfit
+import automol
+import autofile
+from mechanalyzer.inf import spc as sinfo
+from mechanalyzer.inf import thy as tinfo
+
+from mechlib import filesys
+from mechroutines.models import _vib as vib
+from mechroutines.models import ene
+from mechroutines.thermo import basis
+from mechroutines.proc import _util as util
+from mechlib.amech_io import printer as ioprinter
+
+def zmatrix(spc_name, locs, locs_path, cnf_fs):
+    """collect a zmatrix
+    """
+    if cnf_fs[-1].file.geometry.exists(locs):
+        geo = cnf_fs[-1].file.geometry.read(locs)
+        zma = automol.geom.zmatrix(geo)
+        energy = cnf_fs[-1].file.energy.read(locs)
+        comment = 'energy: {0:>15.10f}\n'.format(energy)
+        zma_str = automol.zmat.string(zma)
+    else:
+        zma_str = '\t -- Missing --'
+    spc_data = '\n\nSPC: {}\tConf: {}\tPath: {}\n'.format(
+        spc_name, locs, locs_path) + comment + zma_str
+    return spc_data
+
+
+def geometry(spc_name, locs, locs_path, cnf_fs):
+    """collect a geometry
+    """
+    if cnf_fs[-1].file.geometry.exists(locs):
+        geo = cnf_fs[-1].file.geometry.read(locs)
+        energy = cnf_fs[-1].file.energy.read(locs)
+        comment = 'energy: {0:>15.10f}'.format(energy)
+        xyz_str = automol.geom.xyz_string(geo, comment=comment)
+    else:
+        xyz_str = '\t -- Missing --'
+    spc_data = '\n\nSPC: {}\tConf: {}\tPath: {}\n'.format(
+        spc_name, locs, locs_path) + xyz_str
+    return spc_data
+
+
+def freqs(
+        spc_dct_i, spc_mod_dct_i, proc_keyword_dct, thy_dct,
+        cnf_fs, locs, locs_path, run_prefix, save_prefix):
+    """collect frequencies
+    """
+    if spc_mod_dct_i is not None:
+        pf_filesystems = filesys.models.pf_filesys(
+            spc_dct_i, spc_mod_dct_i,
+            run_prefix, save_prefix, saddle=False)
+        ret = vib.full_vib_analysis(
+            spc_dct_i, pf_filesystems, spc_mod_dct_i,
+            run_prefix, zrxn=None)
+        freqs, _, zpe, sfactor, tors_strs, torsfreqs, all_freqs = ret
+    else:
+        es_levels = util.freq_es_levels(proc_keyword_dct)
+        spc_mod_dct_i = util.generate_spc_model_dct(es_levels, thy_dct)
+        freqs, _, zpe = vib.read_locs_harmonic_freqs(
+            cnf_fs, locs, run_prefix, zrxn=None)
+        if freqs and proc_keyword_dct['scale'] is not None:
+            freqs, zpe = vib.scale_frequencies(
+                freqs, 0.0, spc_mod_dct_i, scale_method='3c')
+        torsfreqs = None
+        all_freqs = None
+        sfactor = None
+    spc_data = [locs_path, zpe, *freqs]
+    return spc_data, (torsfreqs, all_freqs, sfactor)
+
+
+def coeffs(spc_name, spc_dct, model_dct, spc_array):
+    """ get the heat of formation reference molecules for one species.
+    """
+    basis_dct, _ = thermfit.prepare_refs(
+        model_dct['thermfit']['ref_scheme'],
+        spc_dct, (spc_name,))
+    # Get the basis info for the spc of interest
+    spc_basis, coeff_basis = basis_dct[spc_name]
+    coeff_array = []
+    for spc_i in spc_basis:
+        if spc_i not in spc_array:
+            spc_array.append(spc_i)
+    for spc_i in spc_array:
+        if spc_i in spc_basis:
+            coeff_array.append(coeff_basis[spc_basis.index(spc_i)])
+        else:
+            coeff_array.append(0)
+    return [*coeff_array], spc_array
+
+
+def energy(
+    spc_name, spc_dct_i, spc_mod_dct_i, proc_keyword_dct,
+    thy_dct, locs, locs_path,
+    cnf_fs, run_prefix, save_prefix):
+    """ collect energy
+    """
+    energy = None
+    if spc_mod_dct_i:
+        pf_filesystems = filesys.models.pf_filesys(
+            spc_dct_i, spc_mod_dct_i,
+            run_prefix, save_prefix, saddle=False)
+        energy = ene.electronic_energy(
+            spc_dct_i, pf_filesystems, spc_mod_dct_i,
+            conf=(locs, locs_path, cnf_fs))
+    else:
+        spc_info = sinfo.from_dct(spc_dct_i)
+        thy_info = tinfo.from_dct(thy_dct.get(
+            proc_keyword_dct['proplvl']))
+        mod_thy_info = tinfo.modify_orb_label(
+            thy_info, spc_info)
+        sp_save_fs = autofile.fs.single_point(locs_path)
+        sp_save_fs[-1].create(mod_thy_info[1:4])
+        # Read the energy
+        sp_path = sp_save_fs[-1].path(mod_thy_info[1:4])
+        if os.path.exists(sp_path):
+            if sp_save_fs[-1].file.energy.exists(
+                    mod_thy_info[1:4]):
+                ioprinter.reading('Energy', sp_path)
+                energy = sp_save_fs[-1].file.energy.read(
+                    mod_thy_info[1:4])
+    return [locs_path, energy]
+
+
+def enthalpy(
+        spc_name, spc_dct, spc_dct_i, spc_mod_dct_i, model_dct,
+        chn_basis_ene_dct, spc_array, locs, locs_path,
+        cnf_fs, run_prefix, save_prefix):
+    """ collect enthalpies
+    """
+    pf_filesystems = filesys.models.pf_filesys(
+        spc_dct_i, spc_mod_dct_i,
+        run_prefix, save_prefix, saddle=False)
+    ene_abs = ene.read_energy(
+        spc_dct_i, pf_filesystems, spc_mod_dct_i,
+        run_prefix, conf=(locs, locs_path, cnf_fs),
+        read_ene=True, read_zpe=True, saddle=False)
+    hf0k, _, chn_basis_ene_dct, hbasis = basis.enthalpy_calculation(
+        spc_dct, spc_name, ene_abs,
+        chn_basis_ene_dct, model_dct, spc_mod_dct_i,
+        run_prefix, save_prefix, pforktp='pf', zrxn=None)
+    spc_basis, coeff_basis = hbasis[spc_name]
+    coeff_array = []
+    for spc_i in spc_basis:
+        if spc_i not in spc_array:
+            spc_array.append(spc_i)
+    for spc_i in spc_array:
+        if spc_i in spc_basis:
+            coeff_array.append(
+                coeff_basis[spc_basis.index(spc_i)])
+        else:
+            coeff_array.append(0)
+    return [locs_path, ene_abs, hf0k, *coeff_array]
--- mechroutines/proc/tsk.py
+++ mechroutines/proc/tsk.py
@@ -3,10 +3,6 @@
     format
 """
 
-import os
-import autofile
-from mechanalyzer.inf import spc as sinfo
-from mechanalyzer.inf import thy as tinfo
 from mechlib.amech_io import printer as ioprinter
 from mechlib import filesys
 from mechroutines.proc import _util as util
@@ -33,35 +29,34 @@ def run_tsk(tsk, spc_dct, run_lst,
     chn_basis_ene_dct = {}
     spc_array = []
 
-    # print species
+    # Begin the loop over the species
     for spc_name in run_lst:
+
+        # info printed to output file
         ioprinter.obj('line_dash')
         ioprinter.info_message("Species: ", spc_name)
 
         # Heat of formation basis molecules and coefficients
-        # is not conformer specific but for the rest of the tasks
-        # we have to generate a list of conformers to get properties for
+        # does not require filesystem information
         if 'coeffs' in tsk:
             label = spc_name
             csv_data_i, spc_array = collect.coeffs(
                 spc_name, spc_dct, model_dct, spc_array)
             csv_data[label] = csv_data_i
 
+        # All other tasks require filesystem information
         else:
             # unpack spc and level info for conformers
             spc_dct_i = spc_dct[spc_name]
-            thy_info, spc_mod_dct_i = util.choose_theory(
-                proc_keyword_dct, spc_mod_dct_i, thy_dct)
-            rng_cnf_locs_lst, rng_cnf_locs_path = util.choose_conformers(
+            spc_mod_dct_i = util.choose_theory(
+                proc_keyword_dct, spc_mod_dct_i)
+            cnf_fs, rng_cnf_locs_lst, rng_cnf_locs_path = util.choose_conformers(
                 proc_keyword_dct, spc_mod_dct_i,
                 save_prefix, run_prefix, spc_dct_i, thy_dct)
 
             # Loop over conformers
             for locs, locs_path in zip(rng_cnf_locs_lst, rng_cnf_locs_path):
-
                 label = spc_name + '_' + '_'.join(locs)
-                _, cnf_fs = filesys.build_fs(
-                    run_prefix, save_prefix, 'CONFORMER')
                 if 'freq' in tsk:
                     csv_data_i, csv_data_j = collect.freqs(
                         spc_dct_i, spc_mod_dct_i, proc_keyword_dct, thy_dct,
@@ -84,56 +79,19 @@ def run_tsk(tsk, spc_dct, run_lst,
                     csv_data[label] = csv_data_i
 
                 elif 'ene' in tsk:
-                    energy = None
-                    if spc_mod_dct_i:
-                        pf_filesystems = filesys.models.pf_filesys(
-                            spc_dct_i, spc_mod_dct_i,
-                            run_prefix, save_prefix, saddle=False)
-                        energy = ene.electronic_energy(
-                            spc_dct_i, pf_filesystems, spc_mod_dct_i,
-                            conf=(locs, locs_path, cnf_fs))
-                    else:
-                        spc_info = sinfo.from_dct(spc_dct_i)
-                        thy_info = tinfo.from_dct(thy_dct.get(
-                            proc_keyword_dct['proplvl']))
-                        mod_thy_info = tinfo.modify_orb_label(
-                            thy_info, spc_info)
-                        sp_save_fs = autofile.fs.single_point(locs_path)
-                        sp_save_fs[-1].create(mod_thy_info[1:4])
-                        # Read the energy
-                        sp_path = sp_save_fs[-1].path(mod_thy_info[1:4])
-                        if os.path.exists(sp_path):
-                            if sp_save_fs[-1].file.energy.exists(
-                                    mod_thy_info[1:4]):
-                                ioprinter.reading('Energy', sp_path)
-                                energy = sp_save_fs[-1].file.energy.read(
-                                    mod_thy_info[1:4])
-                    csv_data[label] = [locs_path, energy]
+                    csv_data_i = collect.energy(
+                        spc_name, spc_dct_i, spc_mod_dct_i,
+                        proc_keyword_dct, thy_dct, locs, locs_path,
+                        cnf_fs, run_prefix, save_prefix)
+                    csv_data[label] = csv_data_i
 
                 elif 'enthalpy' in tsk:
-                    energy = None
-                    pf_filesystems = filesys.models.pf_filesys(
-                        spc_dct_i, spc_mod_dct_i,
-                        run_prefix, save_prefix, saddle=False)
-                    ene_abs = ene.read_energy(
-                        spc_dct_i, pf_filesystems, spc_mod_dct_i,
-                        run_prefix, conf=(locs, locs_path, cnf_fs),
-                        read_ene=True, read_zpe=True, saddle=False)
-                    hf0k, _, chn_basis_ene_dct, hbasis = basis.enthalpy_calculation(
-                        spc_dct, spc_name, ene_abs,
-                        chn_basis_ene_dct, model_dct, spc_mod_dct_i,
-                        run_prefix, save_prefix, pforktp='pf', zrxn=None)
-                    spc_basis, coeff_basis = hbasis[spc_name]
-                    coeff_array = []
-                    for spc_i in spc_basis:
-                        if spc_i not in spc_array:
-                            spc_array.append(spc_i)
-                    for spc_i in spc_array:
-                        if spc_i in spc_basis:
-                            coeff_array.append(
-                                coeff_basis[spc_basis.index(spc_i)])
-                        else:
-                            coeff_array.append(0)
-                    csv_data[label] = [locs_path, ene_abs, hf0k, *coeff_array]
+                    ret = collect.enthalpy(
+                        spc_name, spc_dct, spc_dct_i, spc_mod_dct_i,
+                        model_dct, chn_basis_ene_dct, spc_array,
+                        locs, locs_path, cnf_fs, run_prefix, save_prefix)
+                    csv_data_i, chn_basis_ene_dct, spc_array = ret
+                    csv_data[label] = csv_data_i
 
+    # write the csv data into the appropriate file
     util.write_csv_data(tsk, csv_data, filelabel, spc_array)
