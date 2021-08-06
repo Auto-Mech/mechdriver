cd420dddd54fd810c64d425385f462ece1c4dba6
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-08-06 01:06:53 -0500

    update prepare_refs calls

--- mechroutines/thermo/basis.py
+++ mechroutines/thermo/basis.py
@@ -145,8 +145,9 @@ def enthalpy_calculation(
     ref_scheme = pes_mod_dct_i['therm_fit']['ref_scheme']
     ref_enes = pes_mod_dct_i['therm_fit']['ref_enes']
 
-    basis_dct, uniref_dct = thermfit.prepare_refs(
+    basis_dct = thermfit.prepare_basis(
         ref_scheme, spc_dct, (spc_name,), zrxn=zrxn)
+    uniref_dct = thermfit.unique_basis_species(basis_dct, spc_dct)
 
     # Get the basis info for the spc of interest
     spc_basis, coeff_basis = basis_dct[spc_name]
@@ -195,8 +196,11 @@ def enthalpy_calculation(
                 ts_ref_scheme = 'cbh' + ref_scheme.split('_')[1]
         if zrxn is None:
             if ref_scheme != ts_ref_scheme:
-                basis_dct_trs, uniref_dct_trs = thermfit.prepare_refs(
+                basis_dct_trs, uniref_dct_trs = thermfit.prepare_basis(
                     ts_ref_scheme, spc_dct, (spc_name,), zrxn=zrxn)
+                uniref_dct_trs = thermfit.unique_basis_species(
+                    basis_dct_trs, spc_dct)
+
                 spc_basis_trs, coeff_basis_trs = basis_dct_trs[spc_name]
                 ene_basis_trs = []
                 energy_missing = False
