e01ae454ae390ff316dea73d24ce0a8b0f1ca3ed
snelliott <sarah.elliott26@uga.edu>
2021-08-23 18:11:14 -0500

    dual level hrscans save to the correct locations now

--- mechroutines/es/_routines/conformer.py
+++ mechroutines/es/_routines/conformer.py
@@ -1089,7 +1089,7 @@ def fs_confs_dict(cnf_save_fs, cnf_save_locs_lst,
     match_dct = {}
     for ini_locs in ini_cnf_save_locs_lst:
 
-        match_dct[ini_locs] = None
+        match_dct[tuple(ini_locs)] = None
         # Loop over structs in cnf_save, see if they match the current struct
         inigeo = ini_cnf_save_fs[-1].file.geometry.read(ini_locs)
         inizma = automol.geom.zmatrix(inigeo)
@@ -1104,7 +1104,7 @@ def fs_confs_dict(cnf_save_fs, cnf_save_locs_lst,
                 cnf_save_path = cnf_save_fs[-1].path(locs)
                 info_message(
                     '- Similar structure found at {}'.format(cnf_save_path))
-                match_dct[ini_locs] = locs
+                match_dct[tuple(ini_locs)] = tuple(locs)
                 break
 
     return match_dct
