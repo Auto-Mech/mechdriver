8b45b6af00bc49c32b18f701b0db6276083c7716
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2021-12-21 15:40:17 -0600

    energy sorting for rates

--- mechroutines/ktp/rates.py
+++ mechroutines/ktp/rates.py
@@ -141,6 +141,9 @@ def make_pes_mess_str(spc_dct, rxn_lst, pes_idx, pesgrp_num,
         run_prefix, save_prefix, ref_idx=0)
     basis_energy_dct[spc_model].update(model_basis_energy_dct)
 
+    print('basis energy dct')
+    print(basis_energy_dct)
+
     # Loop over all the channels and write the MESS strings
     written_labels = []
     for rxn in rxn_lst:
