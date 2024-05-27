93a43ba3dfca0ee7ed19bc6a3f16a3422754224d
avcopan <avcopan@gmail.com>
2024-05-27 11:40:46 -0500

    Fix: CLI command line arguments for automech run
    These needed to be added as arguments or options through the `click` library.

--- automech/cli/__init__.py
+++ automech/cli/__init__.py
@@ -9,13 +9,23 @@ def main():
 
 
 @main.command()
-def run():
-    """Expand stereochemistry for a mechanism
+@click.option('-p', '--path', default='.', show_default=True, help='The job run directory')
+@click.option('-S', '--safemode-off', is_flag=True, help='Turn off safemode?')
+def run(path: str = ".", safemode_off: bool = False):
+    """Central Execution script to launch a MechDriver process which will
+    parse all of the user-supplied input files in a specified directory, then
+    launches all of the requested electronic structure, transport,
+    thermochemistry and kinetics calculations via their associated
+    sub-drivers.
+
+    The job run directory must contain an `inp/` subdirectory with the following
+    required files: run.dat, theory.dat, models.dat, species.csv
     """
-    automech.main()
+    automech.main(path=path, safemode_off=safemode_off)
 
 
 @main.command()
+
 def greetme():
     """Hello world function, for CLI testing purposes"""
     print("Hello, world!")
