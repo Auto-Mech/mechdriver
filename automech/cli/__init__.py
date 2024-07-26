e21d28f344339500c61f061fd3e08cd5bb9c7877
avcopan <avcopan@gmail.com>
2024-07-26 09:31:11 -0500

    New: CLI command to check the log file status

--- automech/cli/__init__.py
+++ automech/cli/__init__.py
@@ -1,6 +1,6 @@
 import click
-from automech.cli import _run
-from automech.cli import _subtasks_setup
+
+from automech.cli import _check_log, _run, _subtasks_setup
 
 
 @click.group()
@@ -10,10 +10,12 @@ def main():
 
 
 @main.command()
-@click.option('-p', '--path', default='.', show_default=True, help='The job run directory')
-@click.option('-S', '--safemode-off', is_flag=True, help='Turn off safemode?')
+@click.option(
+    "-p", "--path", default=".", show_default=True, help="The job run directory"
+)
+@click.option("-S", "--safemode-off", is_flag=True, help="Turn off safemode?")
 def run(path: str = ".", safemode_off: bool = False):
-    """Run central workflow 
+    """Run central workflow
     Central Execution script to launch a MechDriver process which will
     parse all of the user-supplied input files in a specified directory, then
     launches all of the requested electronic structure, transport,
@@ -26,6 +28,19 @@ def run(path: str = ".", safemode_off: bool = False):
     _run.main(path=path, safemode_off=safemode_off)
 
 
+@main.command()
+@click.option(
+    "-p", "--path", default=".", show_default=True, help="The path to the log file"
+)
+def check_log(path: str = "."):
+    """Check an AutoMech log file to see if it succeeded
+
+    The path must point either directly to the log file, or to a directory where the log
+    file is named "out.log"
+    """
+    _check_log.main(path=path)
+
+
 @main.group()
 def subtasks():
     """Run AutoMech subtasks in parallel"""
@@ -33,7 +48,9 @@ def subtasks():
 
 
 @subtasks.command()
-@click.option('-p', '--path', default='.', show_default=True, help='The job run directory')
+@click.option(
+    "-p", "--path", default=".", show_default=True, help="The job run directory"
+)
 def setup(path: str = "."):
     """Set-up subtasks from a user-supplied AutoMech directory
 
