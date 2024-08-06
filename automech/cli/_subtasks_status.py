17342e2cdfde1885dbdd0c9a404b0cab94fba316
avcopan <avcopan@gmail.com>
2024-08-06 14:12:10 -0500

    New: Print non-OK log paths along with status summary

--- automech/cli/_subtasks_status.py
+++ automech/cli/_subtasks_status.py
@@ -2,6 +2,7 @@
 """
 
 import itertools
+from collections.abc import Sequence
 from pathlib import Path
 
 import more_itertools as mit
@@ -46,11 +47,13 @@ def main(
     run_path.mkdir(exist_ok=True)
     save_path.mkdir(exist_ok=True)
 
+    non_okay_log_records = []
     for group_id in group_ids:
         df = pandas.read_csv(path / f"{group_id}.csv")
         tasks = read_task_list(path / f"{group_id}.yaml")
         twidth = task_column_width(tasks)
         skeys = subtask_keys(tasks)
+
         header = task_group_header(skeys, twidth)
         print(header)
         for task_key, row in df.iterrows():
@@ -58,12 +61,44 @@ def main(
             assert row[TableKey.task] == task.name, f"{row} does not match {task.name}"
 
             subtask_paths = list(map(row.get, skeys))
+            subtask_abs_paths = [Path(work_path) / p for p in subtask_paths]
             subtask_stats = list(
-                map(colored_status_string, map(parse_subtask_status, subtask_paths))
+                map(colored_status_string, map(parse_subtask_status, subtask_abs_paths))
             )
             line = f"{task.name:>{twidth}} " + " ".join(subtask_stats)
             print(line)
+
+            non_okay_log_records.extend(
+                (task.name, skey, p, s)
+                for skey, spath in zip(skeys, subtask_paths, strict=True)
+                for p, s in log_paths_with_statuses(
+                    spath, exclude_stats=[Status.OK]
+                ).items()
+            )
         print()
+
+    print(f"Non-OK log files in {work_path}:")
+    twidth = max(len(r[0]) for r in non_okay_log_records)
+    swidth = max(len(r[1]) for r in non_okay_log_records)
+    pwidth = max(len(r[2]) for r in non_okay_log_records)
+    for task_name, skey, log_path, stat in non_okay_log_records:
+        stat = colored_status_string(stat)
+        print(f"{task_name:<{twidth}} {skey:<{swidth}} {log_path:<{pwidth}} {stat}")
+    print()
+
+
+def log_paths_with_statuses(
+    path: str | Path, exclude_stats: Sequence[Status] = ()
+) -> dict[str, Status]:
+    """Get a dictionary of log file paths and statuses at a given path
+
+    :param path: _description_
+    :return: _description_
+    """
+    log_paths = list(map(str, Path(path).glob("out*.log")))
+    log_stats = list(map(parse_log_status, log_paths))
+    log_dct = dict(zip(log_paths, log_stats, strict=True))
+    return {k: v for k, v in log_dct.items() if v not in exclude_stats}
 
 
 def parse_subtask_status(path: str | Path, small_thresh: float = 0.2) -> Status:
@@ -72,11 +107,11 @@ def parse_subtask_status(path: str | Path, small_thresh: float = 0.2) -> Status:
     :param path: The directory path
     :return: The status
     """
-    log_paths = list(Path(path).glob("out*.log"))
-    if not log_paths:
+    log_dct = log_paths_with_statuses(path)
+    if not log_dct:
         return Status.TBD
 
-    log_stats = list(map(parse_log_status, log_paths))
+    log_stats = list(log_dct.values())
     log_stat_set = set(log_stats)
 
     # All log files have the same status -> <common status>
@@ -88,9 +123,8 @@ def parse_subtask_status(path: str | Path, small_thresh: float = 0.2) -> Status:
         return Status.RUNNING
 
     # Some log files have errors -> ERROR | OKAY_1E | OKAY_2E
-    total_count = len(log_stats)
     error_count = log_stats.count(Status.ERROR)
-    error_frac = error_count / total_count
+    error_frac = error_count / len(log_stats)
     if error_count == 1 and error_frac < small_thresh:
         return Status.OK_1E
     if error_count == 2 and error_frac < small_thresh:
