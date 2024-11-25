e07f5e3f522dcb7a5765b26b5b739b668e4a9879
avcopan <avcopan@gmail.com>
2024-11-25 12:17:46 -0600

    Finalizes local testing set-up
    Local tests are run using:
    ```
    pixi run test local <node1> <node2> <...>
    ```
    You can check the status of the local test run using:
    ```
    pixi run test status
    ```
    Finally, you MUST sign off on the current version:
    ```
    pixi run test sign
    ```
    The GitHub actions tests will check that the signed version is current.

--- tests/test_automech.py
+++ tests/test_automech.py
@@ -3,6 +3,7 @@
 
 import contextlib
 import os
+import subprocess
 import sys
 import tarfile
 from pathlib import Path
@@ -14,6 +15,7 @@ import yaml
 TEST_ROOT_DIR = Path(__file__).parent
 TESTS = [t for t in yaml.safe_load((TEST_ROOT_DIR / "config.yaml").read_text())]
 ARCHIVE_FILE = TEST_ROOT_DIR / "archive.tgz"
+SIGNATURE_FILE = TEST_ROOT_DIR / "signature.yaml"
 
 
 class Logger(object):
@@ -31,13 +33,56 @@ class Logger(object):
         pass
 
 
-def test_sign():
+class InvalidSignatureError(Exception):
+    def __init__(self, message, *_):
+        # Call the base class constructor with the parameters it needs
+        lines = [
+            message,
+            "  You need to sign off on your local tests using `pixi run test sign`",
+            "  (This must be done for an up-to-date version of the code)",
+        ]
+        super().__init__("\n".join(lines))
+
+
+def test_signature():
     """Unpack archive and check provenance"""
     os.chdir(TEST_ROOT_DIR)
-    if ARCHIVE_FILE.exists():
-        print(f"Unpacking {ARCHIVE_FILE}...")
-        with tarfile.open(ARCHIVE_FILE, "r") as tar:
-            tar.extractall()
+
+    # 1. Unpack the test archive
+    unpack_archive()
+
+    # 2. Read test provenance from signature file
+    sign_dct = yaml.safe_load(SIGNATURE_FILE.read_text())
+    prov_dct: dict[str, str] = sign_dct["provenance"]
+
+    # 3. Check the commit hash of this repo
+    # (must match most recent commit that isn't a merge PR or test archive update)
+    print("Checking whether mechdriver matches the signed version...")
+    exclude_messages = ("Merge pull request", "Updates tests/archive.tgz")
+    exclude_grep = r"\|".join(exclude_messages)
+    self_repo_prov = subprocess.check_output(
+        ["git", "log", "--oneline", "-1", "--invert-grep", "--grep", exclude_grep],
+        text=True,
+    )
+    self_sign_prov = prov_dct.pop("mechdriver")
+    self_repo_commit = self_repo_prov.split()[0]
+    self_sign_commit = self_sign_prov.split()[0]
+    if not are_equivalent_commit_hashes(self_repo_commit, self_sign_commit):
+        raise InvalidSignatureError(f"{self_repo_commit} !~ {self_sign_commit}")
+
+    # 4. Check the commit hashes of the other repos
+    # (must match most recent commit)
+    for repo, sign_prov in prov_dct.items():
+        print(f"Checking whether {repo} matches the signed version...")
+
+        repo_prov = subprocess.check_output(
+            ["git", "ls-remote", f"https://github.com/Auto-Mech/{repo}.git", "HEAD"],
+            text=True,
+        )
+        repo_commit = repo_prov.split()[0]
+        sign_commit = sign_prov.split()[0]
+        if not are_equivalent_commit_hashes(repo_commit, sign_commit):
+            raise InvalidSignatureError(f"{repo_commit} !~ {sign_commit}")
 
 
 @pytest.mark.parametrize("name", TESTS)
@@ -52,6 +97,29 @@ def test_workflow(name: str):
         automech.run()
 
 
+def unpack_archive() -> None:
+    """Un-tar a directory in its current location
+
+    :param path: The path where the AutoMech subtasks were set up
+    """
+    os.chdir(TEST_ROOT_DIR)
+    if ARCHIVE_FILE.exists():
+        print(f"Unpacking {ARCHIVE_FILE}...")
+        with tarfile.open(ARCHIVE_FILE, "r") as tar:
+            tar.extractall()
+
+
+def are_equivalent_commit_hashes(hash1: str, hash2: str) -> bool:
+    """Check if two git commit hashes are equivalent.
+
+    :param hash1: First hash
+    :param hash2: Second hash
+    :return: `True` if they are, `False` if they aren't
+    """
+    nchars = max(4, min(*map(len, [hash1, hash2])))
+    return hash1[:nchars] == hash2[:nchars]
+
+
 if __name__ == "__main__":
     # test_workflow("quick")
-    test_sign()
+    test_signature()
