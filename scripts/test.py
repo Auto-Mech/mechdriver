9c146fdf790356d428b487febd0103e3a906dd8e
avcopan <avcopan@gmail.com>
2025-08-18 10:27:46 -0500

    Remove test.py worker creation and update README.md

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 """Local testing CLI."""
 
-import contextlib
 import os
-import shutil
 import socket
-import subprocess
 import warnings
-from pathlib import Path
 
 import click
-import pint
-import pyparsing as pp
 
 import mechdriver
 import test_utils as tu
@@ -80,88 +74,6 @@ def local(manager: str | None = None, manager_flags: str | None = None):
 def sign():
     """Sign off on local tests."""
     tu.wrap_up_tests(from_archive=True, allow_override=True)
-
-
-# Create node worker
-WORKER_DIR = Path(".workers")
-
-
-@main.command("create-node-worker")
-@click.argument("host")
-@click.option("-q", "--queue", required=True, help="Queue name")
-@click.option("-A", "--account", required=True, help="Account name")
-def create_node_worker(host: str, queue: str, account: str):
-    """Create a worker for a specific node."""
-    # Create script
-    cpus = host_cpus(host)
-    mem_mib = host_memory(host, unit="MiB")
-    name = f"worker-{host}"
-    script_text = worker_script_text(
-        host, name=name, cpus=cpus, mem_mib=mem_mib, queue=queue, account=account
-    )
-
-    # Submit script
-    WORKER_DIR.mkdir(exist_ok=True)
-    with contextlib.chdir(WORKER_DIR):
-        script_name = f"{name}.sh"
-        script = Path(script_name)
-        script.write_text(script_text)
-        subprocess.run(["qsub", script_name])
-
-
-# Helper functions
-def host_cpus(host: str) -> int:
-    """Determine host number of CPUs."""
-    cpus_query = subprocess.run(
-        ["ssh", host, "nproc --all"], capture_output=True, text=True
-    )
-    return int(cpus_query.stdout)
-
-
-def host_memory(host: str, unit: str = "GB") -> int:
-    """Determine host memory."""
-    mem_query = subprocess.run(
-        ["ssh", host, "grep MemTotal /proc/meminfo"], capture_output=True, text=True
-    )
-    mem_expr = pp.Suppress("MemTotal:") + pp.SkipTo(pp.StringEnd())("mem")
-    mem = pint.Quantity(mem_expr.parse_string(mem_query.stdout).get("mem"))
-    return int(mem.m_as(unit))
-
-
-PBS_SCRIPT = """
-#!/bin/bash
-#PBS -l select=1:host={host}
-#PBS -N {name}
-#PBS -l walltime=01:00:00
-#PBS -q {queue} -A {account}
-
-hq worker start \\
-    --idle-timeout "1h" \\
-    --manager "pbs" \\
-    --cpus "{cpus:d}" \\
-    --resource "mem=sum({mem_mib})" \\
-    --on-server-lost "stop" \\
-    --time-limit "2h"
-
-"""
-
-
-def worker_script_text(
-    host: str, *, name: str, cpus: int, mem_mib: int, queue: str, account: str
-) -> str:
-    """Generate worker script text."""
-    if shutil.which("qsub"):
-        return PBS_SCRIPT.format(
-            host=host,
-            name=name,
-            queue=queue,
-            account=account,
-            cpus=cpus,
-            mem_mib=mem_mib,
-        )
-
-    msg = "Worker script generation currently only implemented for PBS."
-    raise NotImplementedError(msg)
 
 
 if __name__ == "__main__":
