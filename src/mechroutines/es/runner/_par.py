b2ea0415bf452bf1132911dec81beaa44c38d0b7
avcopan <avcopan@gmail.com>
2025-09-01 11:09:54 -0400

    Fix: Incorrect IOp parameters set for revDSD-PBEP86-D3BJ
    See [here](https://www.compchem.me/revdsd-pbep86-functional) and
    [here](https://doi.org/10.1021/acs.jpca.9b03157).

--- src/mechroutines/es/runner/_par.py
+++ src/mechroutines/es/runner/_par.py
@@ -102,7 +102,7 @@ def _gaussian(method_dct, prog, job=None, geo=None, spc_info=None):
         rev_recipe = (
             '# empiricaldispersion=gd3bj '
             'iop(3/125=0079905785,3/78=0429604296,3/76=0310006900,'
-            '3/74=1004,3/174=0437700,3/175=-1,3/176=0,3/177=1,'
+            '3/74=1004,3/174=0437700,3/175=-1,3/176=0,3/177=-1,'
             '3/178=5500000)'
         )
         if 'gen_lines' not in kwargs:
