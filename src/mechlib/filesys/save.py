d248278f1cffbcb5d0616dad0a44b9336007a5d9
snelliott <sarah.elliott26@uga.edu>
2026-01-15 21:24:59 -0600

    cut down on prints

--- src/mechlib/filesys/save.py
+++ src/mechlib/filesys/save.py
@@ -291,7 +291,6 @@ def read_zma_from_geo(init_zma, geo):
     """
     zma = None
     if init_zma is not None:
-        print('Resetting ZMA coords using opt geoms...')
         zma = rebuild_zma_from_opt_geo(init_zma, geo)
     return zma
 
@@ -405,7 +404,6 @@ def _save_zmatrix(ret, zma_fs, zma_locs, init_zma=None):
     inf_obj, inp_str, out_str, prog, _ = _unpack_ret(ret)
     zma = None
     if init_zma is not None:
-        print('using opt geo fro zma')
         geo = elstruct.reader.opt_geometry(prog, out_str)
         zma = read_zma_from_geo(init_zma, geo)
     if zma is None:
